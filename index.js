825770eeeae154306b569fd6286e02c5f77a6729
henry <thanhhaipham652@gmail.com>
2025-08-31 08:40:43 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Write a Javascript program to check two numbers and return true if one of the number is 100 
+// or if the sum of the two numbers is 100 
+
+function checkNum (num1, num2) {
+    if (num1 === 100 || num2 === 100) {
+        console.log(true);
+    } else if (num1 + num2 === 100) {
+        console.log(true);
+    } else {
+        console.log(false)
+    }
+}
+
+
+const checkNum = (num1, num2) => num1 === 100 || num2 === 100 || (num1 + num2) === 100;
+
+
+
+
+
