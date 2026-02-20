b07fe025591a9a84bbc4dea0d06b8a5d4c056024
henry <thanhhaipham652@gmail.com>
2026-02-20 11:28:45 -0800

    Convert JSPractice from submodule to folder

---
+++ JSPractice/todo-list.js
@@ -0,0 +1,51 @@
+let toDoArray = [];
+      let taskInput = document.querySelector('.todo-input1');
+      let addButton = document.querySelector('.add-button');
+      let resetButton = document.querySelector('.reset-button');
+      let dateInput = document.querySelector('.date-input')
+
+      function addTask() {
+        let task = taskInput.value;
+        let date = dateInput.value;
+        if (task !== "" && date !== "") {
+          toDoArray.push({name: task, dueDate: date});
+        } else {
+          alert("You must select a task and a due date!");
+        }
+        taskInput.value = "";
+        renderTask();
+        
+      }
+
+      function resetList() {
+        let toDoList = document.querySelector('.task-display');
+        toDoList.innerHTML = ""
+        toDoArray = [];
+      }
+
+      function renderTask() {
+        let toDoList = document.querySelector('.task-display');
+        toDoList.innerHTML = "";
+        for (let i = 0; i < toDoArray.length; i++) {
+          toDoList.innerHTML += `
+          <div class = "todo-item">
+            <span class = "text">${toDoArray[i].name}</span> 
+            <span class = "date">${toDoArray[i].dueDate}</span>
+            <button class = "delete-button" 
+            onclick = "toDoArray.splice(${i}, 1); renderTask()"
+            >Delete</button> 
+          </div>`;
+        }
+      }
+
+  
+
+      addButton.addEventListener("click", addTask);
+      resetButton.addEventListener("click", resetList);
+
+      
+      taskInput.addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+          addTask();
+        }
+      });
